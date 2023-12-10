cf4bdb28f6b9ff30121a9c4e9a40d3a0c5797b2d
dulang_105583 <dulanga.jayarathne@axiatadigitallabs.com>
2023-12-10 17:21:57 +0530

    removed dynamodb

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 
-from utils.dynamodb_handler import get_repositories, get_all_repositories, get_contributors_in_repository
+# from utils.dynamodb_handler import get_repositories, get_all_repositories, get_contributors_in_repository
 
 app = Flask(__name__)
 
@@ -11,26 +11,26 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
-@app.route('/all_repositories')
-def all_repositories():
-    return get_all_repositories()
+# @app.route('/all_repositories')
+# def all_repositories():
+#     return get_all_repositories()
 
 
-@app.route('/repositories', methods=['GET'])
-def all_repositories_for_day_id():
-    day_id = request.args.get('day_id')
-    return get_repositories(day_id)
+# @app.route('/repositories', methods=['GET'])
+# def all_repositories_for_day_id():
+#     day_id = request.args.get('day_id')
+#     return get_repositories(day_id)
 
 
-@app.route('/contributors', methods=['GET'])
-def get_contributors_in_repo():
-    repo = request.args.get('repo')
-    return get_contributors_in_repository(repo)
+# @app.route('/contributors', methods=['GET'])
+# def get_contributors_in_repo():
+#     repo = request.args.get('repo')
+#     return get_contributors_in_repository(repo)
 
 
-@app.route('/events')
-def get_events():  # put application's code here
-    return 'Hello World!'
+# @app.route('/events')
+# def get_events():  # put application's code here
+#     return 'Hello World!'
 
 
 if __name__ == '__main__':
--- utils/dynamodb_handler.py
+++ utils/dynamodb_handler.py
@@ -1,88 +1,88 @@
-# Author: Dulanga Heshan
-# Date Created: YYYY-MM-DD
-# Date Updated: YYYY-MM-DD
+# # Author: Dulanga Heshan
+# # Date Created: YYYY-MM-DD
+# # Date Updated: YYYY-MM-DD
+# # import boto3
+# from datetime import datetime
+
+# from boto3 import client, resource
+# from botocore.exceptions import NoCredentialsError
+# from decouple import config
+
+# print(config("AWS_ACCESS_KEY_ID"))
+
+# AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+# REGION_NAME = config("REGION_NAME")
 # import boto3
-from datetime import datetime
+# from boto3.dynamodb.conditions import Key
+# from datetime import datetime
 
-from boto3 import client, resource
-from botocore.exceptions import NoCredentialsError
-from decouple import config
+# # DynamoDB client and resource
+# client = boto3.client(
+#     'dynamodb',
+#     aws_access_key_id=AWS_ACCESS_KEY_ID,
+#     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+#     region_name=REGION_NAME
+# )
 
-print(config("AWS_ACCESS_KEY_ID"))
+# resource = boto3.resource(
+#     'dynamodb',
+#     aws_access_key_id=AWS_ACCESS_KEY_ID,
+#     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+#     region_name=REGION_NAME
+# )
 
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-REGION_NAME = config("REGION_NAME")
-import boto3
-from boto3.dynamodb.conditions import Key
-from datetime import datetime
-
-# DynamoDB client and resource
-client = boto3.client(
-    'dynamodb',
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=REGION_NAME
-)
-
-resource = boto3.resource(
-    'dynamodb',
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=REGION_NAME
-)
-
-RepositoriesTable = resource.Table('Repositories')
-UsersTable = resource.Table('Users')
-EventsTable = resource.Table('Events')
+# RepositoriesTable = resource.Table('Repositories')
+# UsersTable = resource.Table('Users')
+# EventsTable = resource.Table('Events')
 
 
-def get_all_repositories():
-    response = RepositoriesTable.scan()
-    data = response['Items']
-
-    while 'LastEvaluatedKey' in response:
-        response = RepositoriesTable.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        data.extend(response['Items'])
-    return response
-
-
-def get_repositories(day_id):
-    print(day_id)
-    filtering_exp = Key("day_id").eq(int(day_id))
-    return RepositoriesTable.query(KeyConditionExpression=filtering_exp)
-
-
-# def get_all_users():
-#     response = UsersTable.scan()
+# def get_all_repositories():
+#     response = RepositoriesTable.scan()
 #     data = response['Items']
-#
+
 #     while 'LastEvaluatedKey' in response:
 #         response = RepositoriesTable.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
 #         data.extend(response['Items'])
 #     return response
 
 
-def get_contributors_in_repository(repository):
-    print(repository)
-    filtering_exp = Key("repository").eq(repository)
-    return UsersTable.query(KeyConditionExpression=filtering_exp)
+# def get_repositories(day_id):
+#     print(day_id)
+#     filtering_exp = Key("day_id").eq(int(day_id))
+#     return RepositoriesTable.query(KeyConditionExpression=filtering_exp)
 
 
-def get_events(partition_key, sort_key):
-    try:
-        response = EventsTable.get_item(
-            Key={
-                'day_id': partition_key,
-                'repository_name': sort_key
-            }
-        )
+# # def get_all_users():
+# #     response = UsersTable.scan()
+# #     data = response['Items']
+# #
+# #     while 'LastEvaluatedKey' in response:
+# #         response = RepositoriesTable.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+# #         data.extend(response['Items'])
+# #     return response
 
-        item = response.get('Item')
-        if item:
-            print('Item found:', item)
-        else:
-            print('Item not found.')
 
-    except NoCredentialsError:
-        print('Credentials not available.')
+# def get_contributors_in_repository(repository):
+#     print(repository)
+#     filtering_exp = Key("repository").eq(repository)
+#     return UsersTable.query(KeyConditionExpression=filtering_exp)
+
+
+# def get_events(partition_key, sort_key):
+#     try:
+#         response = EventsTable.get_item(
+#             Key={
+#                 'day_id': partition_key,
+#                 'repository_name': sort_key
+#             }
+#         )
+
+#         item = response.get('Item')
+#         if item:
+#             print('Item found:', item)
+#         else:
+#             print('Item not found.')
+
+#     except NoCredentialsError:
+#         print('Credentials not available.')
